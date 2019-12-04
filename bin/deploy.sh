d5207cb0fea57c37edccff7dfefcab2a60661a1f
Oscar Reyes <oscarryz@gmail.com>
2019-12-04 04:22:20 +0000

    Hide the menu when deploying to prod

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,4 @@
-cp -r public/css public/js dist/
+cp -r public/css dist/
 cp generated/*.html dist
+echo ".dropdown { display:none; }" >> dist/css/main.css
 node node_modules/gh-pages/bin/gh-pages -d dist
