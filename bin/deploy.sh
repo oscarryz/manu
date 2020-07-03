840a0068aa298320a227750921f1092350e32dff
Oscar Reyes <oscarryz@gmail.com>
2020-07-03 05:17:43 +0000

    Include images on deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,5 @@
-rm dist/*.html dist/css/* 
-cp -r public/css dist/
+rm dist/*.html dist/css/* dist/img/*
+cp -r public/css public/img dist/
 cp generated/*.html dist
 echo ".dropdown { display:none; }" >> dist/css/main.css
 node node_modules/gh-pages/bin/gh-pages -d dist
