c0dbf9c938f013a8e4be3385a44c7c5ea1a5b0f3
oscarryz <oscarryz@gmail.com>
2020-07-16 23:43:26 -0500

    About me

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,5 @@
 rm dist/*.html dist/css/* dist/img/*
-cp -r public/css public/img dist/
+cp -r public/css public/img public/webfonts public/about-me.html dist/
 cp generated/*.html generated/*.json dist
 echo ".dropdown { display:none; }" >> dist/css/main.css
 node node_modules/gh-pages/bin/gh-pages -d dist
