b090aec2d1423eb186ef5105d982b4d4ef236411
Oscar Reyes <oscarryz@gmail.com>
2025-02-27 07:03:15 -0600

    entries listing

--- public/js/main.js
+++ public/js/main.js
@@ -66,17 +66,3 @@ function newEntry() {
   window.location.href = "/new";
 }
 window.onload = autoSave;
-
-function resizeIframe(obj) {
-  obj.style.height = obj.contentWindow.document.body.scrollHeight + 20 + "px";
-}
-
-function toggleTheme() {
-  const newTheme =
-    document.querySelector("html").dataset.theme === "light" ? "dark" : "light";
-  document.querySelector("html").dataset.theme = newTheme;
-  document
-    .querySelector("iframe")
-    .contentDocument.querySelector("html").dataset.theme = newTheme;
-  localStorage.setItem("theme", newTheme);
-}
