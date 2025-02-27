db836913743061b1aecc0450cc421702022887bd
Oscar Reyes <oscarryz@gmail.com>
2025-02-27 06:49:13 -0600

    Move navigation to the top.
    Also fixes scrollbar in entries listing

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,5 @@
 rm dist/*.html dist/css/* dist/img/*
 cp -r public/css public/img public/webfonts public/about-me.html dist/
 cp generated/*.html generated/*.json dist
-echo ".toolbar { display:none; }" >> dist/css/main.css
+echo ".toolbar { visibility: hidden; }" >> dist/css/main.css
 node node_modules/gh-pages/bin/gh-pages -d dist
