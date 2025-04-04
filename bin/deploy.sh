fd008ad2c23abdb7c7776fc2a1c92a3a509ac6a6
Oscar Reyes <oscarryz@gmail.com>
2025-04-04 17:45:27 +0000

    Copies /js files properly when deploying.
    Some files are truncated because they are not needed on view mode.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,9 @@
 rm dist/*.html dist/css/* dist/img/*
-cp -r public/css public/img public/webfonts public/js/entries.js public/about-me.html dist/
+cp -r public/css public/img public/webfonts public/js public/about-me.html dist/
 cp generated/*.html generated/*.json dist
 echo ".toolbar { visibility: hidden; }" >> dist/css/main.css
+echo "" > dist/js/main.js
+echo "" > dist/js/pm.js
+echo "" > dist/js/require-pm.js
+
 node node_modules/gh-pages/bin/gh-pages -d dist
